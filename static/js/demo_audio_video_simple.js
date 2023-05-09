1941a86dbe0b2447877af4a0b4c23a76d602227b
JadeSleeckx <jadesleeckx26@gmail.com>
2023-05-09 09:09:55 +0200

    back to the beginnig

--- static/js/demo_audio_video_simple.js
+++ static/js/demo_audio_video_simple.js
@@ -3,28 +3,19 @@ var selfEasyrtcid = "";
 
 function connect() {
     easyrtc.setVideoDims(640,480);
-    //easyrtc.setRoomOccupantListener(convertListToButtons);
+    easyrtc.setRoomOccupantListener(convertListToButtons);
     easyrtc.easyApp("easyrtc.audioVideoSimple", "selfVideo", ["callerVideo"], loginSuccess, loginFailure);
+}
 
-    //NEw
-    easyrtc.setStreamAcceptor(function(callerEasyrtcid, stream) {
-        var video = document.getElementById("callerVideo");
-        easyrtc.setVideoObjectSrc(video,stream);
-    });
-    easyrtc.setOnStreamClosed(function (callerEasyrtcid) {
-        easyrtc.setVideoObjectSrc(document.getElementById("callerVideo"), "");
-    });
- }
 
-/*
 function clearConnectList() {
     var otherClientDiv = document.getElementById('otherClients');
     while (otherClientDiv.hasChildNodes()) {
         otherClientDiv.removeChild(otherClientDiv.lastChild);
     }
 }
-*/
-/*
+
+
 function convertListToButtons (roomName, data, isPrimary) {
     clearConnectList();
     var otherClientDiv = document.getElementById('otherClients');
@@ -41,7 +32,7 @@ function convertListToButtons (roomName, data, isPrimary) {
         otherClientDiv.appendChild(button);
     }
 }
-*/
+
 
 
 function performCall(otherEasyrtcid) {
@@ -57,15 +48,7 @@ function loginSuccess(easyrtcid) {
     selfEasyrtcid = easyrtcid;
     document.getElementById("iam").innerHTML = "I am " + easyrtc.cleanId(easyrtcid);
 
-    //VERANDERING (toevoeging)
-    var roomName = "Livestream";
-    easyrtc.joinRoom(roomName, null, function(roomName) {
-        console.log("I'm in room: " + roomName);
-    }, function(errorCode,errorText,roomName){
-        easyrtc.showError(errorCode, errorText + ": room name =" + roomName);
-    });
-
-    //performCall(selfEasyrtcid);
+    performCall(selfEasyrtcid);
 }
 
 
