b79e3010fed0deee3c0368b78b6d4e12fd8c6aa1
JadeSleeckx <jadesleeckx26@gmail.com>
2023-05-10 22:15:22 +0200

    naar loader

--- static/js/Start.js
+++ static/js/Start.js
@@ -1,5 +1,7 @@
 setTimeout(function() {
     const nextPage = 'loader';
-  location.assign(`./demos/?&nextPage=${nextPage}`)
+  location.assign(`../loader.html?&nextPage=${nextPage}`)
   }, 7000); // 7000 milliseconden = 7 seconden
   
+
+  //./demos/
