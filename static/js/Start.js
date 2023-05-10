077d520f6a0d62657d1c4752ff2ae5f6943c8307
JadeSleeckx <jadesleeckx26@gmail.com>
2023-05-10 23:19:05 +0200

    naar loader

--- static/js/Start.js
+++ static/js/Start.js
@@ -1,9 +1,7 @@
-setTimeout(function() {
-  const nextPage = 'loader';
-  console.log('Redirecting...');
-  location.assign('../loader.html?&nextPage=' + nextPage);
-  }, 1000); // 7000 milliseconden = 7 seconden
-  
-
-  //./demos/
-  //static\loader.html
+const interval = setInterval(function() {
+  if (document.readyState === 'complete') {
+    clearInterval(interval);
+    const nextPage = 'loader';
+    location.assign('../loader.html?nextPage=' + nextPage);
+  }
+}, 1000);
