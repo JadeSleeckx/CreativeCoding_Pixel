cff17da77b6b50b07c4fb0de0924a021ef268578
JadeSleeckx <jadesleeckx26@gmail.com>
2023-05-10 13:34:48 +0200

    pixel

--- static/js/demo_audio_video_simple.js
+++ static/js/demo_audio_video_simple.js
@@ -1,5 +1,5 @@
 var selfEasyrtcid = "";
-var callerVideo;
+let callerVideo;
 
 function convertListToButtons (roomName, data, isPrimary) {
     clearConnectList();
@@ -30,12 +30,6 @@ function connect() {
     easyrtc.setVideoDims(640,480);
     easyrtc.setRoomOccupantListener(convertListToButtons);
     easyrtc.easyApp("easyrtc.audioVideoSimple", "selfVideo", ["callerVideo"], loginSuccess, loginFailure);
-    //TOEGEVOEGD
-    callerVideo = createCapture(VIDEO);
-    // Set the capture dimensions
-    callerVideo.size(640, 480);
-    // Hide the capture element
-    callerVideo.hide();
 }
 
 
@@ -45,11 +39,6 @@ function clearConnectList() {
         otherClientDiv.removeChild(otherClientDiv.lastChild);
     }
 }
-
-
-
-
-
 
 function performCall(otherEasyrtcid) {
     easyrtc.hangupAll();
@@ -76,36 +65,21 @@ function loginFailure(errorCode, message) {
 // Variable for webcam
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(400, 400);
     // Set the pixel density to 1
     pixelDensity(1);
     // Get the webcam feed
-    callerVideo = createCapture({video: {deviceId: 'some-device-id-here'}});
+    /*
+    callerVideo = createCapture(VIDEO);
+    */
+    callerVideo = document.getElementById('callerVideo');
+    callerVideo.style.display = 'none';
     // Hide the DOM element for the image <video>
     callerVideo.hide();
     noStroke();
-}
-
-function draw() {
-    // Load the webcam capture pixels
-    callerVideo.loadPixels();
-
-    // Draw the pixelated video on a p5.js canvas
-    push();
-    scale(0.25);
-    image(callerVideo, 0, 0, callerVideo.width, callerVideo.height);
-    filter(POSTERIZE, 4);
-    pop();
-
-    // Get the canvas as a video source for EasyRTC
-    var canvas = document.getElementsByTagName('canvas')[0];
-    var video = canvas.captureStream().getVideoTracks()[0];
-
-    // Set the video source for the EasyRTC call
-    easyrtc.setVideoObjectSrc(document.getElementById('callerVideo'), video);
-}
- /*
-function draw() {
+  }
+  
+  function draw() {
     background(220);
     // Display camera image
     image(callerVideo, 0, 0);
@@ -114,14 +88,13 @@ function draw() {
     // Loop through every 10th x and 10th y location
     for(let x = 0; x < callerVideo.width; x+=10) {
      for(let y = 0; y < callerVideo.height; y+=10) {
-        // Get an array of rgba values for each pixel
+       // Get an array of rgba values for each pixel
        // [r,g,b,a]
-        let colorFromVideo = callerVideo.get(x,y);
-        // Get the brightness from the rgba array
-        fill( colorFromVideo );
-        // Draw a 10x10 rectangle
-        rect(x, y, 10, 10);
-        }
+       let colorFromVideo = callerVideo.get(x,y);
+       // Get the brightness from the rgba array
+       fill( colorFromVideo );
+       // Draw a 10x10 rectangle
+       rect(x, y, 10, 10);
+     }
     }    
-}
-*/
+  }
