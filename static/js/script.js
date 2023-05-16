c296af04d1314f435656c6c1cceac8b21a0fe6b7
JadeSleeckx <jadesleeckx26@gmail.com>
2023-05-16 14:34:32 +0200

    ip

--- static/js/script.js
+++ static/js/script.js
@@ -97,8 +97,14 @@ socket.on('prompt', function(prompt) {
 
 var ws;
 
+const urlParams = new URLSearchParams(window.location.search);
+const myParam = urlParams.get('ip');
+
 var wsUri = "ws://192.168.100.1:1880/ws";
 
+if (myParam == "3") {
+    wsUri = "ws://192.168.100.3:1880/ws";
+}
 /*var wsUri = "ws://127.0.0.1/ws";*/
 function wsConnect() {
     console.log("connect", wsUri);
